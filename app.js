b7afafbce54777c6079ce122783956474669ff25
Maire Chew <mairechew@gmail.com>
2018-01-15 10:44:58 -0700

    working through validity implementation

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const getPokerRanking = (cardArray) => {
+const getPokerRanking = cardArray => {
   if (cardArray.length !== 5) {
-    return 'Poker hand must have 5 cards';
-  }
-}
+    return "Poker hand must have 5 cards";
+  };
+  checkIndividualCardValidity(cardArray);
+};
 
+// use regExpr to check that cards are in valid format
+const checkIndividualCardValidity = cardArray => {
+  var validCount = 0;
+  for (var i=0; i<cardArray.length; i++) {
+    if (cardArray[i].charAt(0) !== || cardArray[i].charAt(1) !== ) {
+      return 'One or more cards passed in the array is not in the valid format ex: Ah or Qd';
+    };
+  };
+};
 
-
-const addCardsToObject = (cardArray) => {
+const addCardsToObject = cardArray => {
   var cardObject = {};
 
-
   return cardObject;
-}
+};
 
-const checkMatching = (cardObject) => {
-
-}
+const checkMatching = cardObject => {};
 
 module.exports.getPokerRanking = getPokerRanking;
--- test/test.js
+++ test/test.js
@@ -6,6 +6,12 @@ describe('getPokerRanking', function() {
     assert.equal(getPokerRanking(['Ah','As','10c','7d','6s','8d']), 'Poker hand must have 5 cards');
     assert.equal(getPokerRanking([]), 'Poker hand must have 5 cards');
     assert.equal(getPokerRanking(['Ah','As']), 'Poker hand must have 5 cards');
-
   });
+
+  it('Should only process arrays in correct card format', function() {
+    assert.equal(getPokerRanking([]), 'One or more cards passed in the array is not in the valid format ex: Ah or Qd');
+    assert.equal(getPokerRanking([]), 'One or more cards passed in the array is not in the valid format ex: Ah or Qd');
+  });
+
+
 });
