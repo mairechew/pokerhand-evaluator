6aab6a587e8ed5fb9f6f54ec8d07197f23faf167
Maire Chew <mairechew@gmail.com>
2018-01-17 19:59:16 -0700

    working on mocha tests

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const getPokerRanking = cardArray => {
 };
 
 // use regExpr to check that cards are in valid format
-const checkIndividualCardValidity = cardArray => {
-  var validCount = 0;
-  for (var i=0; i<cardArray.length; i++) {
-    if (cardArray[i].charAt(0) !== || cardArray[i].charAt(1) !== ) {
-      return 'One or more cards passed in the array is not in the valid format ex: Ah or Qd';
-    };
-  };
-};
+// const checkIndividualCardValidity = cardArray => {
+//   var validCount = 0;
+//   for (var i=0; i<cardArray.length; i++) {
+//     if (cardArray[i].charAt(0) !== || cardArray[i].charAt(1) !== ) {
+//       return 'One or more cards passed in the array is not in the valid format ex: Ah or Qd';
+//     };
+//   };
+// };
 
 const addCardsToObject = cardArray => {
   var cardObject = {};
--- test/test.js
+++ test/test.js
@@ -8,10 +8,10 @@ describe('getPokerRanking', function() {
     assert.equal(getPokerRanking(['Ah','As']), 'Poker hand must have 5 cards');
   });
 
-  it('Should only process arrays in correct card format', function() {
-    assert.equal(getPokerRanking([]), 'One or more cards passed in the array is not in the valid format ex: Ah or Qd');
-    assert.equal(getPokerRanking([]), 'One or more cards passed in the array is not in the valid format ex: Ah or Qd');
-  });
+  // it('Should only process arrays in correct card format', function() {
+  //   assert.equal(getPokerRanking([]), 'One or more cards passed in the array is not in the valid format ex: Ah or Qd');
+  //   assert.equal(getPokerRanking([]), 'One or more cards passed in the array is not in the valid format ex: Ah or Qd');
+  // });
 
 
 });
